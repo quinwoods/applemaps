7e420238303f80c618dff91c2ff6c3131818d0ee
Quin'darius <quinwoodz@gmail.com>
2020-02-12 12:06:10 -0500

    Initial Commit

---
+++ applemaps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  applemaps
+//
+//  Created by Quin’darius Lyles-Woods on 2/12/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
