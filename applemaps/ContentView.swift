70701b25473d603520a2c59347a487febb60b45b
Quin'darius <quinwoodz@gmail.com>
2020-02-17 12:23:40 -0500

    Adding custom call outs with the data structure for them

--- applemaps/ContentView.swift
+++ applemaps/ContentView.swift
@@ -7,12 +7,66 @@
 //
 
 import SwiftUI
+import CoreLocation
 
-/// This allows the view to be seen on the phone
+struct Landmark: Equatable {
+    static func ==(lhs: Landmark, rhs: Landmark) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let id = UUID().uuidString
+    let name: String
+    let location: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
-    var body: some View{
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+    @State var landmarks: [Landmark] = [
+        Landmark(name: "Westpark Consumer Recycling Center", location: .init(latitude: 29.725570, longitude: -95.468440)),
+        Landmark(name: "Reuse Warehouse", location: .init(latitude: 29.830060, longitude: -95.398800)),
+         Landmark(name: "Ellington Airport/Clear Lake Recycling Center", location: .init(latitude: 29.593130, longitude: -95.169950)),
+          Landmark(name: "Kingwood Recycling Center ", location: .init(latitude: 30.054080, longitude: -95.185520)),
+          Landmark(name: "Environmental Service Center (ESC) - South", location: .init(latitude: 29.755140, longitude: 95.463150)),
+    ]
+    
+    @State var selectedLandmark: Landmark? = nil
+    
+    var body: some View {
+        ZStack {
+            MapView(landmarks: $landmarks,
+                    selectedLandmark: $selectedLandmark)
+                .edgesIgnoringSafeArea(.vertical)
+            VStack {
+                Spacer()
+                Button(action: {
+                    self.selectNextLandmark()
+                }) {
+                    Text("Next")
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .shadow(radius: 3)
+                        .padding(.bottom)
+                }
+            }
+        }
+    }
+    
+    private func selectNextLandmark() {
+        if let selectedLandmark = selectedLandmark, let currentIndex = landmarks.firstIndex(where: { $0 == selectedLandmark }), currentIndex + 1 < landmarks.endIndex {
+            self.selectedLandmark = landmarks[currentIndex + 1]
+        } else {
+            selectedLandmark = landmarks.first
+        }
     }
 }
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
 
